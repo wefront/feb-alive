3a92eda9e109fd9fe85a7e994b2fcffa6ee01c73
hangaoke1 <188869009@qq.com>
2021-04-30 12:08:18 +0800

    perf: 完善babel配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import { babel } from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-// import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.js',
@@ -17,17 +16,6 @@ export default {
       name: '$febAlive',
       exports: 'auto',
     },
-    // {
-    //   file: 'dist/feb-alive.browser.js',
-    //   format: 'iife',
-    //   name: '$febAlive',
-    // },
-    // {
-    //   file: 'dist/feb-alive.browser.min.js',
-    //   format: 'iife',
-    //   name: '$febAlive',
-    //   plugins: [terser()],
-    // },
   ],
   plugins: [
     nodeResolve(),
