c2b760909033e6ef9e0368411c32fe9e8c984086
Dante <duanjl.china@gmail.com>
2022-03-18 14:22:55 +0800

    fix: 不应将 debug 信息暴露在 window 中

--- src/cache.js
+++ src/cache.js
@@ -3,9 +3,5 @@
  * record page cache
  */
 
-const inBrowser = typeof window !== 'undefined';
 const cache = Object.create(null);
-if (inBrowser) {
-  window.debug_cache = cache;
-}
 export default cache;
